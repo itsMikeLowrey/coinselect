4564732bdd3db71a6f2e37fe4b13b6192e56877a
Karel Bilek <kb@karelbilek.com>
2017-07-02 04:25:55 +0200

    Adding BnB to strategy comparison

--- stats/strategies.js
+++ stats/strategies.js
@@ -41,6 +41,68 @@ function blackrand (utxos, outputs, feeRate) {
   return accumulative(utxos, outputs, feeRate)
 }
 
+function bnbrand (utxos, outputs, feeRate) {
+  // attempt to use the bnb strategy first (no change output)
+  let base = branchandbound(utxos, outputs, feeRate)
+  if (base.inputs) return base
+
+  utxos = shuffle(utxos)
+
+  // else, try the accumulative strategy
+  return accumulative(utxos, outputs, feeRate)
+}
+
+function bnbmin (utxos, outputs, feeRate) {
+  // attempt to use the blackjack strategy first (no change output)
+  let base = branchandbound(utxos, outputs, feeRate)
+  if (base.inputs) return base
+
+  // order by descending value
+  utxos = utxos.concat().sort((a, b) => b.value - a.value)
+
+  // else, try the accumulative strategy
+  return accumulative(utxos, outputs, feeRate)
+}
+
+function bnbmax (utxos, outputs, feeRate) {
+  // attempt to use the bnb strategy first (no change output)
+  let base = branchandbound(utxos, outputs, feeRate)
+  if (base.inputs) return base
+
+  // order by ascending value
+  utxos = utxos.concat().sort((a, b) => a.value - b.value)
+
+  // else, try the accumulative strategy
+  return accumulative(utxos, outputs, feeRate)
+}
+
+function bnbcs (utxos, outputs, feeRate) {
+  // attempt to use the bnb strategy first (no change output)
+  let base = branchandbound(utxos, outputs, feeRate)
+  if (base.inputs) return base
+
+  // else, try the current default
+  return coinSelect(utxos, outputs, feeRate)
+}
+
+function bnbus (utxos, outputs, feeRate) {
+  // order by descending value, minus the inputs approximate fee
+  function utxoScore (x, feeRate) {
+    return x.value - (feeRate * utils.inputBytes(x))
+  }
+
+  // attempt to use the blackjack strategy first (no change output)
+  let base = branchandbound(utxos, outputs, feeRate)
+  if (base.inputs) return base
+
+  utxos = utxos.concat().sort(function (a, b) {
+    return utxoScore(b, feeRate) - utxoScore(a, feeRate)
+  })
+
+  // else, try the accumulative strategy
+  return accumulative(utxos, outputs, feeRate)
+}
+
 function maximal (utxos, outputs, feeRate) {
   utxos = utxos.concat().sort((a, b) => a.value - b.value)
 
@@ -129,7 +191,12 @@ function privet (utxos, outputs, feeRate) {
 module.exports = {
   accumulative,
   bestof,
-  branchandbound,
+  bnb: branchandbound,
+  bnbrand,
+  bnbmin,
+  bnbmax,
+  bnbcs,
+  bnbus,
   blackjack,
   blackmax,
   blackmin,
